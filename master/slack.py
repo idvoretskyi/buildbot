eac0353a48317e19eab052c2f208e953cb522762
Etienne Laurin <atnnn@rethinkdb.com>
2014-07-30 21:44:24 -0700

    debugging slack

--- master/slack.py
+++ master/slack.py
@@ -6,6 +6,7 @@ import requests
 from zope.interface import implements
 from buildbot.status.base import StatusReceiverMultiService
 from buildbot.interfaces import IStatusReceiver
+from twisted.python import log
 
 def link(url, text):
     if text:
@@ -25,6 +26,7 @@ class Slack(StatusReceiverMultiService):
     implements(IStatusReceiver)
 
     def __init__(self, webhook):
+        log.msg('Slack: __init__')
         StatusReceiverMultiService.__init__(self)
         self.webhook = webhook
 
@@ -32,17 +34,21 @@ class Slack(StatusReceiverMultiService):
         if payload is None:
             fields = [('text', text), ('attachments', attachments)]
             payload = {k: v for k, v in fields if v is not None}
+        log.msg('Slack: send ' + json.dumps(payload))
         res = requests.post(self.webhook, data={'payload': json.dumps(payload)})
         res.raise_for_status()
 
     def builderAdded(self, name, builder):
+        log.msg('Slack: builderAdded')
         return self
 
     def startService(self):
+        log.msg('Slack: startService')
         StatusReceiverMultiService.startService(self)
         self._status = self.parent.getStatus()
         self._status.subscribe(self)
 
     def stopService(self):
+        log.msg('Slack: stopService')
         StatusReceiverMultiService.stopService(self)
         self._status.unsubscribe(self)
